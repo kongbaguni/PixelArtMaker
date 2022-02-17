d19a619131d8210ab96ab9c65e25d5b121ea3804
서창열 <kongbaguni@gmail.com>
2022-02-17 14:09:38 +0900

    Initial Commit

---
+++ Shared/PixelArtMakerApp.swift
@@ -0,0 +1,17 @@
+//
+//  PixelArtMakerApp.swift
+//  Shared
+//
+//  Created by Changyeol Seo on 2022/02/17.
+//
+
+import SwiftUI
+
+@main
+struct PixelArtMakerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
