9ec99defd2122f5e36399f4e61f7f2d0e14475ca
서창열 <kongbaguni@gmail.com>
2022-04-26 14:45:46 +0900

    사이즈 수정.

--- Shared/Consts.swift
+++ Shared/Consts.swift
@@ -34,7 +34,10 @@ struct Consts {
     
     static var canvasSizes:[CGFloat] {
         if InAppPurchaseModel.isSubscribe {
-            return [16,24,32,36,48,56,64,72,80,88,96]
+            return (1...20).compactMap { value in
+                return CGFloat(value * 8)
+            }
+//            return [16,24,32,36,48,56,64,72,80,88,96,104,112,120,128]
         } else {
             return [16,36,64]
         }
