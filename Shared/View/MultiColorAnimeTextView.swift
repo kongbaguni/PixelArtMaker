279f42bd669a73187e6bdfe90f2c78ed306ff490
chang-yul, Seo <kongbaguni@gmail.com>
2023-01-20 16:36:27 +0900

    ...

--- Shared/View/MultiColorAnimeTextView.swift
+++ Shared/View/MultiColorAnimeTextView.swift
@@ -13,44 +13,65 @@ struct MultiColorAnimeTextView: View {
     let forgroundColors:[Color]
     let backgroudColors:[Color]
     let millisecond:Int
-    @State var idx = 0
-    @State var isAnimate = false 
-    private var textId:Int {
-        idx % texts.count
+    @State var idx:UInt8 = 0
+    @State var isAnimating: Bool = false
+    var newIdx:Int {
+        Int(Double(idx) / 2)
+    }
+    private var textId:(Int,Int) {
+        (newIdx % texts.count, (newIdx + 1) % texts.count)
     }
     
-    private var fgColorId:Int {
-        idx % forgroundColors.count
+    private var fgColorId:(Int,Int) {
+        (newIdx % forgroundColors.count,(newIdx + 1) % forgroundColors.count)
     }
     
-    private var bgColorId:Int {
-        idx % backgroudColors.count
+    private var bgColorId:(Int,Int) {
+        (newIdx % backgroudColors.count,(newIdx + 1) % backgroudColors.count)
     }
     
-    private var fontId:Int {
-        idx % fonts.count
+    private var fontId:(Int,Int) {
+        (newIdx % fonts.count,(newIdx + 1) % fonts.count)
     }
     
     var body: some View {
-        texts[textId]
-            .font(fonts[fontId])
-            .padding(5)
-            .foregroundColor(forgroundColors[fgColorId])
-            .background(backgroudColors[bgColorId])
-            .onAppear {
-                changeIdx()
-            }
-            .cornerRadius(5)
-            .overlay {
-                RoundedRectangle(cornerRadius: 5)
-                    .stroke(forgroundColors[fgColorId], lineWidth: 4)
-            }
-            .animation(SwiftUI.Animation.easeIn(duration: Double(millisecond) / 1000), value: isAnimate)
+        ZStack {
+            texts[textId.0]
+                .font(fonts[fontId.0])
+                .padding(5)
+                .foregroundColor(forgroundColors[fgColorId.0])
+                .background(backgroudColors[bgColorId.0])
+                .cornerRadius(5)
+                .overlay {
+                    RoundedRectangle(cornerRadius: 5)
+                        .stroke(forgroundColors[fgColorId.0], lineWidth: 4)
+                }
+            
+            texts[textId.1]
+                .font(fonts[fontId.1])
+                .padding(5)
+                .foregroundColor(forgroundColors[fgColorId.1])
+                .background(backgroudColors[bgColorId.1])
+                .cornerRadius(5)
+                .overlay {
+                    RoundedRectangle(cornerRadius: 5)
+                        .stroke(forgroundColors[fgColorId.1], lineWidth: 4)
+                }
+                .opacity((idx % 2 == 0) ? 0.0 : 1.0 )
+                .animation(.easeOut, value: isAnimating)
+
+        }.onAppear {
+            changeIdx()
+            isAnimating = true
+        }
+
     }
     
     private func changeIdx() {
         idx += 1
-        isAnimate = true
+        if idx > (texts.count *  fonts.count * forgroundColors.count * backgroudColors.count * 2)  {
+            idx = 0
+        }
         DispatchQueue.main.asyncAfter(deadline: .now() + .milliseconds(500)) {
             changeIdx()
         }
