4551ffdf0eac82c993db736ebe08a3144356a578
서창열 <kongbaguni@gmail.com>
2023-07-07 16:22:53 +0900

    widget title 수정

--- widget/widget.swift
+++ widget/widget.swift
@@ -15,7 +15,7 @@ struct Provider: IntentTimelineProvider {
     }
 
     func getSnapshot(for configuration: ConfigurationIntent, in context: Context, completion: @escaping (SimpleEntry) -> ()) {
-        let entry = SimpleEntry(date: Date(), image:  nil, configuration: configuration)
+        let entry = SimpleEntry(date: Date(), image:  AppGroup.savedImage, configuration: configuration)
         completion(entry)
     }
 
@@ -45,6 +45,7 @@ struct widgetEntryView : View {
     var entry: Provider.Entry
 
     var body: some View {
+        
         if let img = entry.image {
             Image(uiImage: img)
                 .resizable()
@@ -56,20 +57,23 @@ struct widgetEntryView : View {
 }
 
 struct widget: Widget {
-    let kind: String = "widget"
+    let kind: String = "app title"
 
     var body: some WidgetConfiguration {
         IntentConfiguration(kind: kind, intent: ConfigurationIntent.self, provider: Provider()) { entry in
             widgetEntryView(entry: entry)
         }
-        .configurationDisplayName("My Widget")
-        .description("This is an example widget.")
+        .configurationDisplayName("widget title")
+        .description("widget desc")
     }
 }
 
 struct widget_Previews: PreviewProvider {
     static var previews: some View {
-        widgetEntryView(entry: SimpleEntry(date: Date(), image: nil, configuration: ConfigurationIntent()))
+        widgetEntryView(entry: SimpleEntry(
+            date: Date(),
+            image: UIImage(named: "cat"),
+            configuration: ConfigurationIntent()))
             .previewContext(WidgetPreviewContext(family: .systemSmall))
     }
 }
