07dd8a9dc084af429429a394bab58a8594927e48
서창열 <kongbaguni@gmail.com>
2023-07-21 10:51:48 +0900

    인터넷 접속 해제 되었을 때 딤 스크린 제거

--- Shared/DimLoadingViewController.swift
+++ Shared/DimLoadingViewController.swift
@@ -7,11 +7,13 @@
 
 import Foundation
 import UIKit
+import Reachability
 
 class DimLoadingViewController : UIViewController {
 
     let indicator = UIActivityIndicatorView(frame: UIScreen.main.bounds)
-    
+    let reachability = try? Reachability()
+    var isInternetConnected:Bool? = nil
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .init(white: 0, alpha: 0.8)
@@ -22,6 +24,29 @@ class DimLoadingViewController : UIViewController {
         indicator.frame.size = view.frame.size
         indicator.color = .white
         view.addSubview(indicator)
+        do {
+            if let reachability = reachability {
+                reachability.whenReachable = { [weak self] _ in
+                    self?.isInternetConnected = true
+                }
+                reachability.whenUnreachable = { [weak self] _ in
+                    self?.isInternetConnected = false
+                    DispatchQueue.main.asyncAfter(deadline: .now() + .seconds(5)) {
+                        if self?.isInternetConnected == false {
+                            self?.dismiss(animated: true)
+                        }
+                    }
+                }
+            }
+            try reachability?.startNotifier()
+        } catch {
+            
+        }
+        
+    }
+    
+    deinit {
+        reachability?.stopNotifier()
     }
     
     func show() {
