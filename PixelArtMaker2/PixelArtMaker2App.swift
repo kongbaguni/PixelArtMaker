940ef28cd8343c60306b84c2560f84194135d643
서창열 <kongbaguni@gmail.com>
2024-04-23 15:16:50 +0900

    Initial Commit

---
+++ PixelArtMaker2/PixelArtMaker2App.swift
@@ -0,0 +1,17 @@
+//
+//  PixelArtMaker2App.swift
+//  PixelArtMaker2
+//
+//  Created by Changyeol Seo on 4/23/24.
+//
+
+import SwiftUI
+
+@main
+struct PixelArtMaker2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
